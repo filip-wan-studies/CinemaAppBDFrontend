0e33b43f8121b75ced4139c5a6091664938fb262
Adam Krizar <apocalypsearisen44@gmail.com>
2020-01-18 01:14:43 +0100

    Fixed broken images

--- src/components/Film/Screenings.js
+++ src/components/Film/Screenings.js
@@ -17,7 +17,7 @@ class Screenings extends React.Component {
     }
 
     renderPoster = () => {
-        return <img src = {this.props.film.imdb.Poster} className="img-thumbnail" alt={this.props.film.imdb.Title} />;
+        if(this.props.film.imdb.Poster != 'N/A') return <img src = {this.props.film.imdb.Poster} className="img-thumbnail" />;
     };
 
     renderScreenings = () => {

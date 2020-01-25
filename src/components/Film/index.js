10defc6e76c4ed7ad942ea2d09d2b44b3ff0ccc6
Adam Krizar <apocalypsearisen44@gmail.com>
2020-01-25 18:43:20 +0100

    Color scheme fix

--- src/components/Film/index.js
+++ src/components/Film/index.js
@@ -15,8 +15,8 @@ class Film extends React.Component {
             return (
                 <div className="d-flex flex-column align-items-center position-relative mt-5">
                     <div
-                        className="spinner-border text-success"
-                        style={{ width: '10rem', height: '10rem' }}
+                        className="spinner-border text-danger"
+                        style={{ width: '10rem', height: '10rem'}}
                         role="status"
                     >
                         <span className="sr-only">Loading...</span>

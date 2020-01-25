5e3a5af26da37bb5cd4e6ab89b1515153e379a00
Filip <filip.k.wanat@gmail.com>
2020-01-25 19:05:05 +0100

    Fix LoadingSpinner color scheme

--- src/components/common/LoadingSpinner.js
+++ src/components/common/LoadingSpinner.js
@@ -4,7 +4,7 @@ class LoadingSpinner extends React.Component {
     render() {
         return (
             <div className="d-flex flex-column align-items-center position-relative mt-5">
-                <div className="spinner-border text-success" style={{ width: '10rem', height: '10rem' }} role="status">
+                <div className="spinner-border text-danger" style={{ width: '10rem', height: '10rem' }} role="status">
                     <span className="sr-only">Loading...</span>
                 </div>
             </div>

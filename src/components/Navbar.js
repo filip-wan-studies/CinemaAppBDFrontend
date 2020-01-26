78801383b01fae795df9a1f01ac4bbd182a95d46
Adam Krizar <apocalypsearisen44@gmail.com>
2020-01-26 17:20:53 +0100

    Dependencies + searchbox

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/styleNavbar.css';
+import ReactSearchBox from 'react-search-box'
 
 const Navbar = () => {
     return (
@@ -8,6 +9,16 @@ const Navbar = () => {
             <nav className="navbar sticky-top">
                 <Link to="/">
                     <span className="navbar-brand mb-0 h1">Best Cinema</span>
+                </Link>
+                <Link to="/">
+                    <ReactSearchBox
+                        placeholder="Film title"
+                        inputBoxBorderColor = "black"
+                        inputBoxColor = "blue"
+                        searchAsYouType={true}
+                        /*data={this.data}*/
+                        //callback={record => console.log(record)}
+                    />
                 </Link>
             </nav>
             <p></p>
